1ac9664aea7e0649a4fa9ecccb201438e00048f1
Dixon Garcia <dixon@softwarecriollo.com>
2017-09-26 22:38:51 -0400

    commit for heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path    = require("path");
+
+var app = express();
+
+/* Asing port 8080 */
+app.set('port', (process.env.PORT || 8080))
+app.listen(app.get('port'));
+
+/* File static */
+app.use(express.static(path.join(__dirname, '/')));
+
+/* All petition send index */
+app.get('*', (req, res) => {
+ res.type('html');
+ res.sendFile(path.join(__dirname+'/index.html'));
+});
